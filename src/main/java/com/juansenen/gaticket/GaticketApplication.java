4a07aa4ee461ca2fc46ccc95e4acf0098265c5cb
Juan Senen <juansenen@hotmail.com>
2023-09-25 19:02:04 +0200

    Start project

---
+++ src/main/java/com/juansenen/gaticket/GaticketApplication.java
@@ -0,0 +1,13 @@
+package com.juansenen.gaticket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GaticketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GaticketApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/juansenen/gaticket/GaticketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.juansenen.gaticket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GaticketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
