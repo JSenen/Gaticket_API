0758ac0b85a8960dddaeaf8ac428bbc5aff4accf
Juan Senen <juansenen@hotmail.com>
2023-10-22 00:20:06 +0200

    Logger and properties and WARN view fixit

--- src/main/java/com/juansenen/gaticket/controller/IncidenceController.java
+++ src/main/java/com/juansenen/gaticket/controller/IncidenceController.java
@@ -6,6 +6,8 @@ import com.juansenen.gaticket.exception.EntityNotFound;
 import com.juansenen.gaticket.service.IncidenceService;
 import com.juansenen.gaticket.service.UserService;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -17,6 +19,8 @@ import java.util.List;
 @Tag(name = "incidence", description = "This controller contains all the endpoints that can manage incidences")
 public class IncidenceController {
 
+    private final static Logger logger = LoggerFactory.getLogger(Incidences.class);
+
     @Autowired
     private IncidenceService incidenceService;
     @Autowired
@@ -24,6 +28,7 @@ public class IncidenceController {
 
     @GetMapping("/incidences")
     public ResponseEntity<List<Incidences>> getALl(){
+        logger.info("/incidences getAll()");
         List<Incidences> incidencesList = incidenceService.findAll();
         return ResponseEntity.ok(incidencesList);
     }
@@ -32,6 +37,7 @@ public class IncidenceController {
     public ResponseEntity<Incidences> addOne(@PathVariable("idUser") long idUser, @RequestBody Incidences incidence) throws EntityNotFound {
         try {
             //Obtener el usuario correspondiente
+            logger.info("/incidences/{idUser} addOne()");
             User userIncidence = userService.findById(idUser);
 
             //Asociar la incidencia con el usuario
@@ -44,6 +50,7 @@ public class IncidenceController {
             return ResponseEntity.status(HttpStatus.CREATED).body(incidence);
         } catch (EntityNotFound ex) {
             // Manejo de excepciones en caso de que el usuario no se encuentre
+            logger.error("/incidence/{idUser} addOne() USER NO FOUND");
             return new ResponseEntity<>(HttpStatus.NOT_FOUND);
         }
 
